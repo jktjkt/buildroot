612ff0a44580069e363ba26508ec296f393512d7
Jan Kundrát <jan.kundrat@cesnet.cz>
2017-03-17 20:35:11 +0100

    HACK: manual "installation" of sysrepo's YANG and XML files

--- package/sysrepo/sysrepo.mk
+++ package/sysrepo/sysrepo.mk
@@ -17,4 +17,21 @@ ifeq ($(BR2_INIT_SYSTEMD),y)
 SYSREPO_CONF_OPTS += -DWITH_SYSTEMD=ON
 endif
 
+define SYSREPO_COPY_YANG_FILES
+	mkdir -p $(TARGET_DIR)/usr/share/yang-for-install/01-sysrepo
+	cp \
+		$(@D)/yang/ietf-netconf-acm@2012-02-22.yang \
+		$(@D)/yang/ietf-netconf-notifications.yang \
+		$(@D)/yang/nc-notifications.yang \
+		$(@D)/examples/yang/turing-machine.yang \
+		$(@D)/examples/yang/turing-machine.data.xml \
+		$(@D)/examples/yang/iana-if-type.yang \
+		$(@D)/examples/yang/ietf-ip@2014-06-16.yang \
+		$(@D)/examples/yang/ietf-interfaces@2014-05-08.yang \
+		$(@D)/examples/yang/ietf-interfaces.data.xml \
+		$(TARGET_DIR)/usr/share/yang-for-install/01-sysrepo/
+endef
+
+SYSREPO_POST_INSTALL_TARGET_HOOKS += SYSREPO_COPY_YANG_FILES
+
 $(eval $(cmake-package))
